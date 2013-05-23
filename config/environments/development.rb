63d070562f0b54b40e4e0bf36b914d0aed260045
Greg Pallotta <gregpallotta@gmail.com>
2013-05-23 19:45:48 -0400

    add localhost:3000 as default url for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ ApartmentV2::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # define default url for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end

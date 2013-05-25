0afc49ad3fea4584788348c32a17be9a1e923ff1
Greg Pallotta <gregpallotta@gmail.com>
2013-05-25 11:26:50 -0400

    scopes are now class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
 
-  has_many :debts_owed_to, class_name: "Debt",
-      foreign_key: "user_owed_to_id", dependent: :destroy
-  has_many :debts_they_owe, class_name: "Debt",
-      foreign_key: "user_who_owes_id", dependent: :destroy
+  has_many :debts_owed_to,
+            class_name: "Debt",
+            foreign_key: "user_owed_to_id",
+            dependent: :destroy
+
+  has_many :debts_they_owe,
+            class_name: "Debt",
+            foreign_key: "user_who_owes_id",
+            dependent: :destroy
+
   has_many :comments
   has_many :chores
   belongs_to :group

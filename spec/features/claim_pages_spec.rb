9f868a6cf296c068695d75f8085d33c3e5757e07
Greg Pallotta <gregpallotta@gmail.com>
2013-05-29 15:03:34 -0400

    only current user claims are shown on claim page

--- spec/features/claim_pages_spec.rb
+++ spec/features/claim_pages_spec.rb
@@ -23,22 +23,35 @@ describe "claim pages" do
                       user_who_owes: user1, paid: true)}
       before { visit claims_path }
 
-      it "displays all unpaid claims related to you by default" do
-        expect(page).to have_content(cl.title)
-        expect(page).to have_content(cl2.title)
-        expect(page).not_to have_content(cl3.title)
+      context "claims within your group" do
+        it "displays all unpaid claims related to you by default" do
+          expect(page).to have_content(cl.title)
+          expect(page).to have_content(cl2.title)
+          expect(page).not_to have_content(cl3.title)
+        end
+        it "displays the title of a claim" do
+          expect(page).to have_content(cl.title)
+        end
+        it "displays the amount of a claim" do
+          expect(page).to have_content(cl.amount)
+        end
+        it "has a link to view a single debt" do
+          expect(page).to have_link('View', href: claim_path(cl))
+        end
+        it "has a link to mark a debt as paid" do
+          expect(page).to have_link('Mark paid', href: mark_as_paid_claim_path(cl))
+        end
       end
-      it "displays the title of a claim" do
-        expect(page).to have_content(cl.title)
-      end
-      it "displays the amount of a claim" do
-        expect(page).to have_content(cl.amount)
-      end
-      it "has a link to view a single debt" do
-        expect(page).to have_link('View', href: claim_path(cl))
-      end
-      it "has a link to mark a debt as paid" do
-        expect(page).to have_link('Mark paid', href: mark_as_paid_claim_path(cl))
+
+      context "claims from other groups" do
+        let(:other_group) { FactoryGirl.create(:group) }
+        let(:other_user) { FactoryGirl.create(:user, group: group) }
+        let!(:other_claim) { FactoryGirl.create(:claim, user_owed_to: other_user,
+                              user_owed_to: other_user)}
+        it "does not show claims from other groups" do
+          visit claims_path
+          expect(page).not_to have_content(other_claim.title)
+        end
       end
     end
 

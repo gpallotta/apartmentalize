66147348cb4777723c00388e91cef6192a94ee9e
Greg Pallotta <gregpallotta@gmail.com>
2013-05-31 15:35:34 -0400

    move manager specs to separate files

---
+++ spec/features/managers/creating_managers_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "creating a new manager" do
+
+  let(:group) { FactoryGirl.create(:group) }
+  let(:user) { FactoryGirl.create(:user, group: group) }
+
+  before do
+    sign_in user
+    visit group_path(group)
+    click_link 'Add An Important Person'
+  end
+
+  context "with invalid info" do
+    it "does not create a manager" do
+      expect { click_button 'Create' }.not_to change { Manager.count }
+    end
+  end
+
+  context "with valid info" do
+
+    before do
+      fill_in 'Title', with: 'Awesome Dude'
+      fill_in 'manager_name', with: 'Dude'
+      fill_in 'Address', with: '123 Blueberry Lane'
+      fill_in 'Phone Number', with: '1234567890'
+    end
+
+    it "creates a manager" do
+      expect { click_button 'Create' }.to change { Manager.count }.by(1)
+    end
+
+    it "displays the newly created manager" do
+      click_button 'Create'
+      expect(page).to have_content('Awesome Dude')
+    end
+
+  end
+
+end

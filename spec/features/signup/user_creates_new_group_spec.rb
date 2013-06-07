6b480909124f6e03a19a4ac3e2cec42f9e24a011
Greg Pallotta <gregpallotta@gmail.com>
2013-06-07 11:58:30 -0400

    finish adding user stories to specs

--- spec/features/signup/user_creates_new_group_spec.rb
+++ spec/features/signup/user_creates_new_group_spec.rb
@@ -1,42 +1,54 @@
+###############
+
+# As a user
+# I want to be able to create a new group
+# so I can create a link for my roommates and I
+
+# AC:
+# I can create a new group
+# I can enter an identifier to uniquely identiy the group
+# I can use a provided default identifier
+# The new group is associated with my account
+
+###############
+
+
 require 'spec_helper'
 
-describe "signing up with joining an existing group" do
+describe "signing up with creating a new group" do
 
-  describe "creating the group" do
-    before { visit new_group_path }
+  before { visit new_group_path }
 
-    describe "with invalid info" do
-      context "when the identifier has been taken" do
-        let!(:group) { FactoryGirl.create(:group) }
-        before do
-          fill_in 'group_identifier', with: group.identifier
-          click_button 'Create New Group'
-        end
+  describe "with invalid info" do
+    context "when the identifier has been taken" do
+      let!(:group) { FactoryGirl.create(:group) }
+      before do
+        fill_in 'group_identifier', with: group.identifier
+        click_button 'Create New Group'
+      end
 
-        it "does not create the group" do
-          expect(page).to have_content('has already been taken')
-          expect(current_path).to eql(groups_path)
-        end
+      it "does not create the group" do
+        expect(page).to have_content('has already been taken')
+        expect(current_path).to eql(groups_path)
+      end
+    end
+  end
+
+  describe "with valid info" do
+    context "entering an identifier to use" do
+      it "creates a group with the provided identifier" do
+        fill_in 'group_identifier', with: 'my_identifier'
+        expect { click_button 'Create New Group' }.to change { Group.count }.by(1)
+        expect(Group.last.identifier).to eql('my_identifier')
       end
     end
 
-    describe "with valid info" do
-      context "entering an identifier to use" do
-        it "creates a group with the provided identifier" do
-          fill_in 'group_identifier', with: 'my_identifier'
-          expect { click_button 'Create New Group' }.to change { Group.count }.by(1)
-          expect(Group.last.identifier).to eql('my_identifier')
-        end
-      end
-
-      context "leaving the identifier blank and using default" do
-        it "creates a group with a default identifier" do
-          expect { click_button 'Create New Group' }.to change { Group.count }.by(1)
-          expect(Group.last.identifier).not_to be_blank
-        end
+    context "leaving the identifier blank and using default" do
+      it "creates a group with a default identifier" do
+        expect { click_button 'Create New Group' }.to change { Group.count }.by(1)
+        expect(Group.last.identifier).not_to be_blank
       end
     end
-
   end
 
 end
--- spec/features/signup/user_joins_existing_group_spec.rb
+++ spec/features/signup/user_joins_existing_group_spec.rb
@@ -1,40 +1,42 @@
+###############
+
+# As a user
+# I want to join an existing group
+# so I can link up with my roommates on the site
+
+# AC:
+# I can enter an existing group identifier
+# I am linked up correctly with the group
+# I am notified if I enter an invalid identifier
+
+###############
+
+
 require 'spec_helper'
 
-describe "signing up with creating a new group" do
+describe "signing up with joining an existing group" do
 
-  describe "sign up link" do
-    it "takes the user to the new group page" do
-      visit welcome_page_path
-      click_link 'Sign up'
-      expect(current_path).to eql(new_group_path)
+  before { visit new_group_path }
+
+  context "when the identifier does not exist" do
+    it "brings the user back to the new group page" do
+      fill_in 'lookup_identifier', with: 'not here'
+      click_button 'Lookup'
+      expect(page).to have_content("Join existing group")
+      expect(page).to have_content("Group not found")
     end
   end
 
-  describe "looking up group" do
-
-    before { visit new_group_path }
-
-    context "when the identifier does not exist" do
-      it "brings the user back to the new group page" do
-        fill_in 'lookup_identifier', with: 'not here'
-        click_button 'Lookup'
-        expect(page).to have_content("Join existing group")
-        expect(page).to have_content("Group not found")
-      end
+  context "when the identifier exists" do
+    before do
+      FactoryGirl.create(:group, identifier: 'exists')
+      fill_in 'lookup_identifier', with: 'exists'
+      click_button 'Lookup'
     end
 
-    context "when the identifier exists" do
-      before do
-        FactoryGirl.create(:group, identifier: 'exists')
-        fill_in 'lookup_identifier', with: 'exists'
-        click_button 'Lookup'
-      end
-
-      it "displays the group identifier on the page" do
-        should have_content('exists')
-      end
+    it "displays the group identifier on the page" do
+      should have_content('exists')
     end
-
   end
 
 end

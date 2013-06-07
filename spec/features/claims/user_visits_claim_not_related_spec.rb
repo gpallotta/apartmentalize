7698b0d4dc6d11b99df4a88da28471aed2ec6997
Greg Pallotta <gregpallotta@gmail.com>
2013-06-07 11:41:48 -0400

    users cannot visit claim page of claims not related to them

--- spec/features/claims/user_visits_claim_not_related_spec.rb
+++ spec/features/claims/user_visits_claim_not_related_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe "user visiting claim page of unrelated claim" do
-  pending
+
+  extend ClaimsHarness
+  create_factories_and_sign_in
+  let!(:cl3) { FactoryGirl.create(:claim, user_owed_to: user3,
+                user_who_owes: user2) }
+
+  it "redirects back to the claims path" do
+    visit claim_path(cl3)
+    expect(current_path).to eql(claims_path)
+  end
+
 end

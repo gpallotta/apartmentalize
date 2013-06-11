43a93cce459b7416858532668b74cb465b549539
Greg Pallotta <gregpallotta@gmail.com>
2013-06-11 14:21:35 -0400

    implement AJAX comment creation using JSON

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :content, :user_id, :created_at, :edit_link, :parsed_time
+  has_one :user
+
+  def parsed_time
+    object.created_at.strftime("%B %d, %Y")
+  end
+
+  def edit_link
+    '<a href="/comments/#{object.id}/edit">Edit Comment</a>'
+  end
+
 end

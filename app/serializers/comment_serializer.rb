c01a847eaa0439f622f5ec041be1c55b35107e4b
Greg Pallotta <gregpallotta@gmail.com>
2013-06-11 23:04:32 -0400

    implement handlebars for rendering new comment

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,13 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :created_at, :edit_link, :parsed_time
+  attributes :id, :content, :user_id, :created_at, :parsed_time
   has_one :user
 
   def parsed_time
     object.created_at.strftime("%B %d, %Y")
-  end
-
-  def edit_link
-    '<a href="/comments/#{object.id}/edit">Edit Comment</a>'
   end
 
 end

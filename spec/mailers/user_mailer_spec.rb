27f615e0eac51810879e7183218860811c078606
Greg Pallotta <gregpallotta@gmail.com>
2013-06-12 15:11:51 -0400

    Add email-spec, refactor user_mailer unit test
    - use email-spec in user_mailer_spec.rb

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,18 +1,20 @@
 require "spec_helper"
 
 describe UserMailer do
+
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
   describe "signup_welcome" do
     let!(:user) { FactoryGirl.create(:user, email: 'to@example.org')}
     let(:mail) { UserMailer.signup_welcome(user) }
 
-    it "renders the headers" do
-      mail.subject.should eq("Welcome to Apartment")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+    it "delivers to the correct email" do
+      expect(mail).to deliver_to('to@example.org')
     end
 
-    it "renders the body" do
-      mail.body.encoded.should match("Welcome to Apartment")
+    it "renders the body and subject" do
+      expect(mail).to have_subject('Welcome to Apartment')
+      expect(mail).to have_body_text('Thanks for signing up')
     end
   end
 

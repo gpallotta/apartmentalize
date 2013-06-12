ba77cab726ff6d6797407eb98a4ad5ff885dd708
Greg Pallotta <gregpallotta@gmail.com>
2013-06-12 13:54:52 -0400

    refactor usage of templates in comment creation
    - create app/templates directory
    - create _comment.html.erb template
    - yield this template in layout
    - call template from this file
    - move relevant javascript to template file

--- app/assets/javascripts/claims.js
+++ app/assets/javascripts/claims.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  $('.search-wrapper').hide();
   $('.user-label, .include-paid, .user-checkbox-label').click(function() {
     $(this).toggleClass('active');
   });
@@ -16,53 +17,18 @@ $(document).ready(function() {
     showClaimForm();
   });
 
-  var comment_source = $('#comment-template').html();
-  var template = Handlebars.compile(comment_source);
-
-  $('#new_comment').submit(function(e) {
-    e.preventDefault();
-    submitCommentForm(this, template);
-  });
-
 });
 
-function increaseCommentCount() {
-  var num = $('#comment-number').html();
-  num = parseInt(num, 10);
-  $('#comment-number').html(num+1);
-}
-
-function submitCommentForm(form_submitted, template) {
-  var form = $(form_submitted);
-  $.ajax({
-    url: form.attr('action') + '.json',
-    type: "POST",
-    data: form.serialize(),
-    cache: false,
-    dataType: "JSON",
-    success: function(result) {
-      var html = template(result);
-      $('.comments-list').append(html);
-      $('#comment-form-errors').hide();
-      $('#new_comment')[0].reset();
-      increaseCommentCount();
-    },
-    error: function() {
-      $('#comment-form-errors').show();
-    }
-  });
+function showClaimForm() {
+  $('.search-wrapper').hide(300);
+  $('.claim-form-wrapper').show(300);
+  $('.create-button').addClass('active');
+  $('.search-button').removeClass('active');
 }
 
 function showSearchForm() {
-  $('.search-wrapper').removeClass("hidden");
-  $('.claim-form-wrapper').addClass("hidden");
+  $('.claim-form-wrapper').hide(300);
+  $('.search-wrapper').show(300);
   $('.create-button').removeClass("active");
   $('.search-button').addClass("active");
-}
-
-function showClaimForm() {
-  $('.search-wrapper').addClass("hidden");
-  $('.claim-form-wrapper').removeClass("hidden");
-  $('.create-button').addClass('active');
-  $('.search-button').removeClass('active');
 }

8927514d2eae677a6698fa74de6b7bce8dc612af
Greg Pallotta <gregpallotta@gmail.com>
2013-06-17 18:09:15 -0400

    add test for a user sending an invitation

--- spec/features/invitations/user_receives_invitation_spec.rb
+++ spec/features/invitations/user_receives_invitation_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
-feature 'User receives invitation'
+feature 'User receives invitation', %q{
+  As a user who has yet to sign up
+  I want to receive an invitation to the site
+  so that I can automatically link up with my roommates
+} do
+
+# AC
+# * I receive an email sent by my roommate to the specific email
+# * When I click a link in the email, I am automatically associated with my roommates
+# * I can specify a name and password after I click the link
+
+end
 

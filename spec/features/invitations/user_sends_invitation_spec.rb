2869d41542d23df2acfcb0f049c670c46949e1f1
Greg Pallotta <gregpallotta@gmail.com>
2013-06-17 17:53:32 -0400

    add test for user entering invalid invite info

--- spec/features/invitations/user_sends_invitation_spec.rb
+++ spec/features/invitations/user_sends_invitation_spec.rb
@@ -1,7 +1,27 @@
-As a user signing up
-I want to be able to invite my roommates after I sign up
-so I can do everything in one step
+require 'spec_helper'
 
+feature 'User sends invitation', %q{
+  As a user signing up
+  I want to be able to invite my roommates after I sign up
+  so I can invite then to use the site with me
+} do
 
-AC:
-I am taken to a page where I can enter my roommates emails after I sign up
+# AC:
+# I am taken to a page where I can enter my roommates emails after I sign up
+
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    sign_in user
+    visit new_user_invitation_path
+  end
+
+  scenario 'user enters valid info'
+
+  scenario 'user enters invalid info' do
+    before_count = User.count
+    click_button 'Send an invitation'
+    expect(User.count).to eql(before_count)
+    expect(page).to have_content("can't be blank")
+  end
+
+end

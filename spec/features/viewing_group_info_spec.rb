f9422cd00d07ea61d6b0ff637099e9d8ed7c8f9c
Greg Pallotta <gregpallotta@gmail.com>
2013-06-18 12:07:25 -0400

    refactor group info spec

--- spec/features/viewing_group_info_spec.rb
+++ spec/features/viewing_group_info_spec.rb
@@ -1,40 +1,25 @@
-###############
-
-# As a user
-# I want to see a list of others in the group
-# so I know who is using the site
-
-# AC:
-# I can see a list of users in my group
-# I can see their contact information
-
-###############
-
 require 'spec_helper'
 
-describe "group index page" do
+feature 'user seeing group info', %q{
+  As a user
+  I want to see a list of others in the group
+  so I know who is using the site with me
+} do
+
+  # AC:
+  # I can see a list of users in my group
+  # I can see their contact information
 
   let(:group) { FactoryGirl.create(:group) }
   let(:user1) { FactoryGirl.create(:user, group: group) }
   let!(:user2) { FactoryGirl.create(:user, group: group) }
 
-  before do
+  scenario 'user visits group show page' do
     sign_in user1
     visit group_path(group)
-  end
-
-  describe "viewing information" do
-
-    context "group info" do
-      it "displays the group identifier" do
-        expect(page).to have_content(group.identifier)
-      end
-      it "displays other members of the group" do
-        expect(page).to have_content(user2.name)
-        expect(page).to have_content(user2.email)
-      end
-    end
-
+    expect(page).to have_content(group.identifier)
+    expect(page).to have_content(user2.name)
+    expect(page).to have_content(user2.email)
   end
 
 end

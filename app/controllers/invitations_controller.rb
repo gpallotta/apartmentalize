d02c378db109dfedad5670e75a43985b7b9894ac
Greg Pallotta <gregpallotta@gmail.com>
2013-06-18 17:30:12 -0400

    replace call to group.id with group_id

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,7 @@
 class InvitationsController < Devise::InvitationsController
 
   def create
-    resource_params[:group_id] = current_user.group.id
+    resource_params[:group_id] = current_user.group_id
     super
   end
 

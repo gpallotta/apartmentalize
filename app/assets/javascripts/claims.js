258b45ca7b2c6e87843746909663a7f59c45b1ed
Greg Pallotta <gregpallotta@gmail.com>
2013-06-18 20:18:21 -0400

    refactor claim paid index spec, add test for ajax failure

--- app/assets/javascripts/claims.js
+++ app/assets/javascripts/claims.js
@@ -61,6 +61,7 @@ function markClaimPaid(link, f) {
       f(result, link);
     },
     error: function() {
+      alert('hi');
       $('#mark-as-paid-error').text('Something went wrong');
     }
   });

26c70b7e3ac3fcf248598f65c1cb46ea02a12d9c
Greg Pallotta <gregpallotta@gmail.com>
2013-06-19 18:43:34 -0400

    refactor comments.js to use MV

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -13,7 +13,36 @@ function Comment() {
 
   var that = this;
 
-  this.addComment = function(comment) {
+  this.commentView = new CommentView();
+
+  this.submitCommentForm = function(form_submitted) {
+    var form = $(form_submitted);
+    $.ajax({
+      url: form.attr('action') + '.json',
+      type: "POST",
+      data: form.serialize(),
+      cache: false,
+      dataType: "JSON",
+      success: function(result) {
+        that.commentView.displayComment(result);
+      },
+      error: function() {
+        that.commentView.displayError();
+      }
+    });
+  };
+
+}
+
+function CommentView() {
+
+  var that = this;
+
+  this.displayError = function() {
+    $('#comment-form-errors').show();
+  };
+
+  this.displayComment = function(comment) {
     html = HandlebarsTemplates['comments/create'](comment);
     $('.comments-list').append(html);
     $('.comments-list div.row:last').hide().fadeIn();
@@ -26,27 +55,6 @@ function Comment() {
     var num = $('#comment-number').html();
     num = parseInt(num, 10);
     $('#comment-number').html(num+1);
-  };
-
-  this.submitCommentForm = function(form_submitted) {
-    var form = $(form_submitted);
-    $.ajax({
-      url: form.attr('action') + '.json',
-      type: "POST",
-      data: form.serialize(),
-      cache: false,
-      dataType: "JSON",
-      success: function(result) {
-        that.addComment(result);
-      },
-      error: function() {
-        that.displayError();
-      }
-    });
-  };
-
-  this.displayError = function() {
-    $('#comment-form-errors').show();
   };
 
 }

fd9c1717c08b5846e738429d97f0807ab17cefa3
Greg Pallotta <gregpallotta@gmail.com>
2013-06-19 16:23:23 -0400

    test AJAX comment creation

--- spec/javascripts/creating_comments_spec.js
+++ spec/javascripts/creating_comments_spec.js
@@ -2,21 +2,46 @@
 
 describe("creating comments", function() {
 
+  beforeEach(function() {
+    $('#konacha').append(HandlebarsTemplates['spec/creating_comment']());
+  });
+
   afterEach(function() {
     $.ajax.restore();
   });
 
+  var claim_object = {
+    comment: {
+      content: 'This is a test',
+      parsed_time: 'Today',
+      user: {
+        name: 'Greg'
+      }
+    }
+  };
+
   describe("success", function() {
     it("creates a comment and displays it", function() {
+      $('#comment_content').val('This is a test');
       sinon.stub($, 'ajax').yieldsTo('success', claim_object);
       comment = new Comment();
-      comment.submitCommentForm( FORM GOES HERE );
+      comment.submitCommentForm($('#new_comment'));
+      expect( $('#comment-form-errors').is(":hidden")).to.be.true;
+
+      var name = $('.comments-list span:first').text().replace(/\s/g,'');
+      expect(name).to.eql('Greg');
+
+      var date = $('.comments-list span:nth-child(2)').text().replace(/\s/g,'');
+      expect(date).to.eql('Today');
+
+      expect( $('.comment-content').text()).to.eql('This is a test');
     });
   });
 
   describe("error", function() {
     it("displays error text", function() {
-
+      sinon.stub($, 'ajax').yieldsTo('error');
+      expect( $('#comment-form-errors').is(":hidden")).to.be.false;
     });
   });
 

08e473d0cd94de50ff0275819064acde9bb91011
Greg Pallotta <gregpallotta@gmail.com>
2013-06-19 13:14:10 -0400

    get JS tests passing after handlebars refactor

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,6 +1,6 @@
-if defined?(Konacha)
-  require 'capybara/poltergeist'
-  Konacha.configure do |config|
-    config.driver = :poltergeist
-  end
-end
+# if defined?(Konacha)
+#   require 'capybara/poltergeist'
+#   Konacha.configure do |config|
+#     config.driver = :poltergeist
+#   end
+# end

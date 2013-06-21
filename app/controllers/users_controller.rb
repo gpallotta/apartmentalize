ce76b27e9233bc9808f70a341ec433783705aab8
Greg Pallotta <gregpallotta@gmail.com>
2013-06-21 11:51:56 -0400

    style group info page, add filter to check other user profile visit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate_user!
+  before_filter :other_user_profile
 
   def show
+  end
+
+  protected
+
+  def other_user_profile
+    redirect_to home_page_path if params[:id] != current_user.id
   end
 
 end
--- spec/features/users/viewing_user_info_spec.rb
+++ spec/features/users/viewing_user_info_spec.rb
@@ -1,36 +1,35 @@
-###############
-
-# As a user
-# I want to view my information on the site
-# so I can see if it is up to date
-
-# AC:
-# I can view my information when I am logged in
-
-###############
-
-
 require 'spec_helper'
 
-describe "viewing user information" do
-  let!(:user) { FactoryGirl.create(:user) }
+feature 'user viewing information', %q{
+  As a user
+  I want to view my information on the site
+  so I can see if it is up to date
+} do
+
+  # AC:
+  # I can view my information when I am logged in
+
+  given(:user) { FactoryGirl.create(:user) }
+
   before do
     sign_in user
     visit user_path(user)
   end
 
-  it "shows the user's info" do
+  scenario 'user visits profile page' do
     expect(page).to have_content(user.name)
     expect(page).to have_content(user.email)
     expect(page).to have_content(user.group.identifier)
-  end
-
-  it "links to the user edit page" do
     expect(page).to have_link('Edit', href: edit_user_registration_path(user))
+    expect(page).to have_link('Group Info', href: group_path(user.group) )
   end
 
-  it "links to the group info page" do
-    expect(page).to have_link('Group Info', href: group_path(user.group) )
+  scenario 'user visits profile page of another user' do
+    user2 = FactoryGirl.create(:user)
+    sign_out user
+    sign_in user2
+    visit user_path(user)
+    expect(current_path).to eql home_page_path
   end
 
 end

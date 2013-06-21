d54484a6435a6658a23c385099e740df813ac2cc
Greg Pallotta <gregpallotta@gmail.com>
2013-06-21 15:11:58 -0400

    remove claim_links presenter, add functionality to claim decorator

--- app/decorators/claim_decorator.rb
+++ app/decorators/claim_decorator.rb
@@ -1,13 +1,53 @@
 class ClaimDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+
+  def paid_on
+    if object.paid?
+      "#{parse_time(object.paid_on)}"
+    else
+      "Unpaid"
+    end
+  end
+
+  def mark_as_paid_link_id
+    "#{object.id}-mark-paid-link"
+  end
+
+  def edit_link user
+    if object.user_owed_to.id == user.id
+      show_edit_link
+    else
+      h.content_tag(:a, 'Cannot edit', class: 'btn disabled')
+    end
+  end
+
+  def mark_as_paid_link user
+    if object.user_owed_to.id == user.id
+      show_mark_as_paid_link
+    else
+      h.content_tag(:a, 'Cannot mark as paid', class: 'btn disabled')
+    end
+  end
+
+
+  protected
+
+  def show_edit_link
+    if object.paid?
+      h.content_tag(:a, 'Cannot edit paid claims', class: 'btn disabled')
+    else
+      h.link_to 'Edit', h.edit_claim_path(object), class: 'btn edit-btn'
+    end
+  end
+
+  def show_mark_as_paid_link
+    if object.paid?
+      h.content_tag(:a, 'Already paid', class: 'btn disabled')
+    else
+      h.link_to 'Mark as paid', h.mark_as_paid_claim_path(object), method: 'put',
+            class: 'btn show-page-mark-paid', :data => { :id => object.id }
+    end
+  end
 
 end
--- spec/decorators/claim_decorator_spec.rb
+++ spec/decorators/claim_decorator_spec.rb
@@ -1,4 +1,62 @@
 require 'spec_helper'
 
 describe ClaimDecorator do
+
+  let(:group) { FactoryGirl.create(:group) }
+  let(:user1) { FactoryGirl.create(:user, group: group )}
+  let(:user2) { FactoryGirl.create(:user, group: group )}
+  let(:claim) { FactoryGirl.create(:claim, user_owed_to: user1, user_who_owes: user2) }
+  let(:decorator) { ClaimDecorator.new(claim) }
+
+  describe "methods" do
+
+    describe ".paid_on" do
+      it "returns 'unpaid' if the claim is unpaid" do
+        expect(decorator.paid_on).to eql('Unpaid')
+      end
+      it "returns the time the claim was paid if it is paid" do
+        claim.mark_as_paid
+        expect(decorator.paid_on).to eql(Time.now.utc.strftime("%B %d, %Y"))
+      end
+    end
+
+    describe ".mark_as_paid_link_id" do
+      it "returns a string with the id of the claim" do
+        expect(decorator.mark_as_paid_link_id).to include("#{claim.id}")
+      end
+    end
+
+    describe ".edit_link" do
+      context "when the user viewing the claim is the one owed to" do
+        it "returns a dead link if the claim is paid" do
+          claim.mark_as_paid
+          expect(decorator.edit_link(user1)).to include('Cannot edit paid claims')
+        end
+        it "returns a link to the edit claim path if the claim is unpaid" do
+          expect(decorator.edit_link(user1)).to include("/claims/#{claim.id}/edit")
+        end
+      end
+
+      context "when the user viewing the claim is one who owes" do
+        it "returns text indicating the user cannot edit" do
+          expect(decorator.edit_link(user2)).to include('Cannot edit')
+        end
+      end
+    end
+
+    describe ".mark_as_paid_link" do
+      context "when the user viewing the claim is the one owed to" do
+        it "returns a dead link if the claim is paid" do
+          claim.mark_as_paid
+          expect(decorator.mark_as_paid_link(user1)).to include('Already paid')
+        end
+        it "returns a mark as paid link if the claim is unpaid" do
+          expect(decorator.mark_as_paid_link(user1)).
+              to include("/claims/#{claim.id}/mark_as_paid")
+        end
+      end
+    end
+
+  end
+
 end

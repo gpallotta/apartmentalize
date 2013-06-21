a6864cd397f08f9288d3792b71e259b34c9bc3ff
Greg Pallotta <gregpallotta@gmail.com>
2013-06-20 21:07:28 -0400

    add tests for donation submission js

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -2,7 +2,7 @@
 //= require_tree ./libraries
 //= require claims
 //= require comments
+//= require donations
 //= require handlebars
 //= require_tree ../../app/assets/javascripts/templates
 //= require_tree ./spec_templates
-//= require donations
--- spec/javascripts/user_sends_donation_spec.js
+++ spec/javascripts/user_sends_donation_spec.js
@@ -2,30 +2,35 @@
 
 describe("user sends a donation", function() {
 
+  var response = {
+    "id": "tok_20mdE9oiA80MZ6"
+  };
+
   beforeEach(function() {
     $('#konacha').append(HandlebarsTemplates['spec_templates/donations']());
-  });
-
-  describe("with a valid credit card", function() {
-    it("returns a valid token and submits form", function() {
-      var card = {
-        number: $('#card_number').val(),
-        cvc: $('#card_code').val(),
-        expMonth: $('#card_month').val(),
-        expYear: $('#card_year').val()
-      };
-    });
+    Stripe.setPublishableKey("pk_test_pcf69croxO3kQTlJ3Csy2yQd");
   });
 
   describe("with an invalid credit card", function() {
     it("returns an invalid token and displays errors", function() {
-      // $('#stripe-error').hide();
       Stripe.setPublishableKey("pk_test_pcf69croxO3kQTlJ3Csy2yQd");
       donation.processCard();
       setTimeout(function() {
         expect($('#stripe-error').text()).to.eql('This card number looks invalid');
       }, 5000);
+    });
+  });
 
+  describe("with a valid credit card", function() {
+    it("returns a valid token and submits form", function() {
+      $('#card_number').val('4242424242424242');
+      $('#card_code').val('123');
+
+      donation.processCard();
+      setTimeout(function() {
+        expect( $('#donation_stripe_card_token').val() ).to.be(undefined);
+      }, 5000);
+      alert($('#donation_stripe_card_token').val());
     });
   });
 

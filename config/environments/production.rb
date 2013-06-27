a8904390b8a9ea5cdc4edcc815fdadfd58cbd0b3
Greg Pallotta <gregpallotta@gmail.com>
2013-06-27 08:52:36 -0400

    fix typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,8 @@ ApartmentV2::Application.configure do
   config.action_mailer.default_url_options = { host: "apartmentalize.info" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME']
-    :password => ENV['SENDGRID_PASSWORD']
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'apartmentalize.info',
     :address => 'smtp.sendgrid.net',
     :port => 587,
